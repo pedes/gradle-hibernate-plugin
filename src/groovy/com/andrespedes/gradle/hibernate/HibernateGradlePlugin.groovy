13a47c1db516d5f662d84c91fc977d9953234ca2
Andres Pedes Morales <andrespedes@gmail.com>
2015-09-29 22:49:55 -0500

    Hibernate Gradle Plugin
    Plugin to do hibernate artifacts generation with Gradle.

---
+++ src/groovy/com/andrespedes/gradle/hibernate/HibernateGradlePlugin.groovy
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package groovy.com.andrespedes.gradle.hibernate
+
+/**
+ * @author Andres
+ *
+ */
+class HibernateGradlePlugin implements Plugin<Project> {
+
+	/**
+	 * 
+	 */
+	public HibernateGradlePlugin() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ test/groovy/com/andrespedes/gradle/hibernate/test/SetUpTest.groovy
@@ -0,0 +1,24 @@
+package groovy.com.andrespedes.gradle.hibernate.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+class SetUpTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
